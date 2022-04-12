536458f0457d3a2653d12177538e0884e50852f2
Ahmed Dardery <Megadardery@yahoo.com>
2022-04-12 17:34:20 +0200

    allow remove overrides

--- src/components/options/Option.js
+++ src/components/options/Option.js
@@ -7,9 +7,7 @@ const trashHandler = (state, label, onChange) => {
 const Option = ({tooltip, label, unit, state, onChange, children}) => {
   return (
     <InputGroup title={tooltip} size="sm" className="mb-1">
-      {
-        //<InputGroup.Text><a className={state._ENABLED ? "trash-button" : "trash-button-x"} onClick={() => trashHandler(state, label, onChange)}></a></InputGroup.Text>
-      }
+      <InputGroup.Text><a className={state._ENABLED ? "trash-button" : "trash-button-x"} onClick={() => trashHandler(state, label, onChange)}></a></InputGroup.Text>
       <InputGroup.Text><label className={state._ENABLED ? "enabled-text" : "disabled-text"} htmlFor={label}>{label}: </label></InputGroup.Text>
       {children}
       {unit && <InputGroup.Text>{unit}</InputGroup.Text>}
